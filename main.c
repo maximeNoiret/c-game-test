3af70d49e7f36ca673d64912689de431b6b34b68
NOIRET Maxime <maxime.noiret@etu.univ-amu.fr>
2026-01-23 20:11:44 +0100

    dev: Crazy centered text
    Fun fact: back when I did a C++ game, I used SETW to center text. Good
    times.

--- main.c
+++ main.c
@@ -1,7 +1,23 @@
+#define CLEAR "\033[2J"
+
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+const unsigned TERMINAL_HEIGHT = 24;
+const unsigned TERMINAL_WIDTH  = 80;
+
+char *setPos(char *buf, unsigned x, unsigned y) {
+	snprintf(buf, 11, "\033[%d;%dH", y, x);
+	return buf;
+}
 
 int main() {
-	printf("This is gonna become a main menu, hopefully.\n");
+	char buf[15];
+	char *text = "This is a test.";
+	unsigned l = strlen(text);
+	unsigned c = (TERMINAL_WIDTH >> 1) - (l >> 1);
+	unsigned h = (TERMINAL_HEIGHT >> 1) - 1;
+	printf(CLEAR"%s%s\n", setPos(buf, c, h), text);
 	return 0;
 }
