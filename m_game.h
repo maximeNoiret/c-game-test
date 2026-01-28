6198f662d6f57c6a6bbad6e8ff66337f5a3f7c2d
NOIRET Maxime <maxime.noiret@etu.univ-amu.fr>
2026-01-28 15:25:49 +0100

    doc: Documented m_game functions

--- m_game.h
+++ m_game.h
@@ -12,10 +12,28 @@ typedef struct GameState {
   Player player;
 } GameState;
 
-
+/**
+ * @brief Allocates space in the heap for the game map matrix.
+ * @param h Desired height of the map.
+ * @param w Desired width of the map.
+ * @return Pointer to the allocated matrix, NULL if allocation failed.
+ */
 char **gamemap_init(const unsigned h, const unsigned w);
+
+/**
+ * @brief Frees the allocated game map matrix.
+ * @param gs GameState that contains the map
+ * @param h Height of the map
+ * @param w Width of the map
+ */
 void gamemap_free(GameState *gs, const unsigned h, const unsigned w);
 
+/**
+ * @brief Process the user input.
+ * @param gs GameState
+ * @param c User input in the form of a char
+ * @return false is Ctrl+D, true otherwise
+ */
 bool process_input(GameState *gs, const char c);
 
 #endif // M_GAME_H_
